0167059b396df7a43104e4f2b996eda3c52a3e60
Jhonn-Vieira <jhonnatavieiradesouza124@gmail.com>
2025-06-05 20:55:21 -0300

    feat(tecnologias): adiciona uma versão para mobile

--- src/JS/script.js
+++ src/JS/script.js
@@ -18,23 +18,50 @@ $(document).ready(function(){
         origin: "right",
         duration: 2000,
         distance: '20%'
+        
     });
 
     ScrollReveal().reveal('#tecnologias_row-2', {
         origin: "left",
         duration: 2000,
+        distance: '20%',
+        delay: 300
+        
+    });
+
+    ScrollReveal().reveal('#tecnologias_row-3', {
+        origin: "left",
+        duration: 2000,
         distance: '20%'
+        
+    });
+    ScrollReveal().reveal('#tecnologias_row-4', {
+        origin: "right",
+        duration: 2000,
+        distance: '20%',
+        delay: 300
+        
+    });
+    ScrollReveal().reveal('#tecnologias_row-5', {
+        origin: "left",
+        duration: 2000,
+        distance: '20%',
+        delay: 500
+        
     });
 
     ScrollReveal().reveal('#qualidades_row-1', {
         origin: "left",
         duration: 1500,
         distance: '20%'
+        
     });
 
      ScrollReveal().reveal('#qualidades_row-2', {
         origin: "right",
         duration: 1500,
-        distance: '20%'
+        distance: '20%',
+        reset: true
+        
     });
 });
